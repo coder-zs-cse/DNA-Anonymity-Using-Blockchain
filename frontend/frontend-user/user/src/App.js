95de6578d030faaa44f69392d400e34c2352acf7
Niranjan <niranjanhebli77@gmail.com>
2022-11-27 02:26:44 +0530

    -a

--- frontend/frontend-user/user/src/App.js
+++ frontend/frontend-user/user/src/App.js
@@ -1,33 +1,33 @@
 // import logo from './logo.svg';
 import "./App.css";
 import React from "react";
-import { HashRouter as Router, Route, Link, NavLink } from "react-router-dom";
+import Form from "./Component/Form";
+import Main from "./Component/Main";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 import imgbg from "./Assets/genedx-bioinformatics-web.jpg";
 function App() {
   return (
-    <>
+<Router>
+  
       <div className="App">
-        <div className="App__Aside">
-          <img src={imgbg} alt="" className="imgcontainer" />
-        </div>
-        <div className="App__Form">
-          <div className="centre">
-            <h1>User Portal</h1>
-          </div>
-          <div className="container">
-            {/* <Link to="/transaction"> */}
-            <button className="FormField__Button">Add New transaction</button>
-            {/* </Link> */}
-          </div>
-          <div className="container margin-bottom:50px ">
-            <button className="FormField__Button ">Update Transaction</button>
-          </div>
-          <div className="container margin-bottom:50px">
-            <button className="FormField__Button ">Display Blockchain</button>
-          </div>
-        </div>
+
+        <Routes>
+          {/* /user --> Component-1
+          /user/someComponet --> Component-2 */}
+          <Route exact path="/" element={<Main/>}/>
+          <Route path="/transaction" element={<Form/>}/>
+
+        </Routes>
       </div>
-    </>
+      
+</Router>
   );
 }
 
